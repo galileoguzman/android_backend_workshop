a2cfd735a9a00a7b04b54903b8e3f22e88a9300d
Galileo Guzman <galileoguzman@gmail.com>
2017-02-28 18:52:48 -0600

    added urls for crud model movie

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -5,4 +5,7 @@ from views import *
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^new/$', movie_new, name='movie_new'),
+    url(r'^edit/(?P<id>\d+)/$', movie_edit, {}, 'movie_edit'),
+    url(r'^delete/(?P<id>\d+)/$', movie_delete, name='opinion_delete'),
 ]
