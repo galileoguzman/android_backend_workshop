f099f433e1040e0b1c5adb22b9417b4a3ebfba3a
Galileo Guzman <galileoguzman@gmail.com>
2017-02-28 19:35:26 -0600

    delete function

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^new/$', movie_new, name='movie_new'),
     url(r'^edit/(?P<id>\d+)/$', movie_edit, {}, 'movie_edit'),
-    url(r'^delete/(?P<id>\d+)/$', movie_delete, name='opinion_delete'),
+    url(r'^delete/(?P<id>\d+)/$', movie_delete, name='movie_delete'),
 ]
